d97b3fb8d8b1391873cd3d38fafde3c86c70efd8
lichunyang_18010089 <lichunyang@shinhofood.com>
2020-09-15 13:41:18 +0800

    net

--- app/src/main/java/com/shinho/android/utils/NetUtils.java
+++ app/src/main/java/com/shinho/android/utils/NetUtils.java
@@ -1,7 +1,154 @@
 package com.shinho.android.utils;
 
-public class NetUtils {
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
 
+import androidx.annotation.RequiresPermission;
 
+import static android.Manifest.permission.ACCESS_NETWORK_STATE;
 
+/**
+ * 网络工具类
+ */
+public final class NetUtils {
+
+    public static final String STATE_NONE = "None";
+    public static final String STATE_WIFI = "WIFI";
+    public static final String STATE_2G = "2G";
+    public static final String STATE_3G = "3G";
+    public static final String STATE_4G = "4G";
+    public static final String STATE_MOBILE = "Mobile";
+
+    private NetUtils() {
+        /* cannot be instantiated */
+        throw new UnsupportedOperationException("cannot be instantiated");
+    }
+
+    /**
+     * 判断网络是否连接
+     */
+    @RequiresPermission(ACCESS_NETWORK_STATE)
+    public static boolean isConnected(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (null == cm) {
+            return false;
+        }
+
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        if (null != info && info.isConnected()) {
+            if (info.getState() == NetworkInfo.State.CONNECTED) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 获取当前网络连接类型
+     *
+     * @return WIFI / 2G ...
+     */
+    @RequiresPermission(ACCESS_NETWORK_STATE)
+    public static String getNetworkState(Context context) {
+        //获取系统的网络服务
+        ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        //如果当前没有网络
+        if (null == connManager) return "";
+
+        //获取当前网络类型，如果为空，返回无网络
+        NetworkInfo activeNetInfo = connManager.getActiveNetworkInfo();
+        if (activeNetInfo == null || !activeNetInfo.isAvailable()) {
+            return STATE_NONE;
+        }
+
+        // 判断是不是连接的是不是wifi
+        NetworkInfo wifiInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (null != wifiInfo) {
+            NetworkInfo.State state = wifiInfo.getState();
+            if (null != state)
+                if (state == NetworkInfo.State.CONNECTED || state == NetworkInfo.State.CONNECTING) {
+                    return STATE_WIFI;
+                }
+        }
+
+        // 如果不是wifi，则判断当前连接的是运营商的哪种网络2g、3g、4g等
+        NetworkInfo networkInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+        if (null != networkInfo) {
+            NetworkInfo.State state = networkInfo.getState();
+            String strSubTypeName = networkInfo.getSubtypeName();
+            if (null != state)
+                if (state == NetworkInfo.State.CONNECTED || state == NetworkInfo.State.CONNECTING) {
+                    switch (activeNetInfo.getSubtype()) {
+                        //如果是2g类型
+                        case TelephonyManager.NETWORK_TYPE_GPRS: // 联通2g
+                        case TelephonyManager.NETWORK_TYPE_CDMA: // 电信2g
+                        case TelephonyManager.NETWORK_TYPE_EDGE: // 移动2g
+                        case TelephonyManager.NETWORK_TYPE_1xRTT:
+                        case TelephonyManager.NETWORK_TYPE_IDEN:
+                            return STATE_2G;
+                        //如果是3g类型
+                        case TelephonyManager.NETWORK_TYPE_EVDO_A: // 电信3g
+                        case TelephonyManager.NETWORK_TYPE_UMTS:
+                        case TelephonyManager.NETWORK_TYPE_EVDO_0:
+                        case TelephonyManager.NETWORK_TYPE_HSDPA:
+                        case TelephonyManager.NETWORK_TYPE_HSUPA:
+                        case TelephonyManager.NETWORK_TYPE_HSPA:
+                        case TelephonyManager.NETWORK_TYPE_EVDO_B:
+                        case TelephonyManager.NETWORK_TYPE_EHRPD:
+                        case TelephonyManager.NETWORK_TYPE_HSPAP:
+                            return STATE_3G;
+                        //如果是4g类型
+                        case TelephonyManager.NETWORK_TYPE_LTE:
+                            return STATE_4G;
+                        default:
+                            //中国移动 联通 电信 三种3G制式
+                            if (strSubTypeName.equalsIgnoreCase("TD-SCDMA") || strSubTypeName.equalsIgnoreCase("WCDMA") || strSubTypeName.equalsIgnoreCase("CDMA2000")) {
+                                return STATE_3G;
+                            } else {
+                                return STATE_MOBILE;
+                            }
+                    }
+                }
+        }
+        return STATE_NONE;
+    }
+
+    /**
+     * 判断是否是wifi连接
+     */
+    @RequiresPermission(ACCESS_NETWORK_STATE)
+    public static boolean isWifi(Context context) {
+        ConnectivityManager cm = (ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (null == cm) {
+            return false;
+        }
+
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        if (null != info) {
+            if (info.getType() == ConnectivityManager.TYPE_WIFI) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    /**
+     * 打开网络设置界面
+     */
+    public static void openSetting(Activity activity, int requestCode) {
+        Intent intent = new Intent(Settings.ACTION_WIRELESS_SETTINGS);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivityForResult(intent, requestCode);
+    }
 }
